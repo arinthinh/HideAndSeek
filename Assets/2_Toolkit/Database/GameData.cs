82d92afb39b0a8dafb75a7c23345aa7f3cb6c949
Phạm Phúc Thịnh <phuccthiinh@gmail.com>
2023-11-08 17:33:12 +0700

    Fix init game dataa

--- Assets/2_Toolkit/Database/GameData.cs
+++ Assets/2_Toolkit/Database/GameData.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [Serializable]
 public class GameData
 {
+    public GameData()
+    {
+        currentLevel = 1;
+        fruits = 0;
+        skinOwned = new() { 0 };
+    }
+
     public int currentLevel;
     public int fruits;
     public List<int> skinOwned;
