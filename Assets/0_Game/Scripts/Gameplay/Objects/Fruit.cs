e62fd70b86d5c030895a2859f83b30725cd84bd7
Phạm Phúc Thịnh <phuccthiinh@gmail.com>
2023-11-09 20:05:05 +0700

    Add stuff

--- Assets/0_Game/Scripts/Gameplay/Objects/Fruit.cs
+++ Assets/0_Game/Scripts/Gameplay/Objects/Fruit.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class Fruit : TriggerMapObject
 {
     [SerializeField] protected Animator _animator;
     
-    protected override void HandleAfterTrigger()
+    protected override async void HandleAfterTrigger()
     {
         base.HandleAfterTrigger();
         _animator.Play("Fruit_Disappear");
+        await UniTask.Delay(500, DelayType.DeltaTime, cancellationToken: this.destroyCancellationToken);
+        gameObject.SetActive(false);
     }
 }
